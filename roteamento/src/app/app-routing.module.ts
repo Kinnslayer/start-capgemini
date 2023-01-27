aba295c068cfd4a8a969799068dfc679eb00da85
Kinnslayer <rodrigo.s.alencar@hotmail.com>
2023-01-27 11:48:41 -0300

    roteamento em angular

---
+++ roteamento/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+
+const routes: Routes = [
+  { path: "primeira-pagina", component: PrimeiraPaginaComponent },
+  { path: "segunda-pagina", component: SegundaPaginaComponent },
+  { path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent},
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  { path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent},
+  { path: "**", component: PaginaNaoEncontradaComponent }
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
