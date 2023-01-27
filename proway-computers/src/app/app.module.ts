8ad9ae035fe7096f58a172e936b3bcb54fcca8d9
Kinnslayer <rodrigo.s.alencar@hotmail.com>
2023-01-27 18:26:01 -0300

    execução website com angular

---
+++ proway-computers/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRootModule } from './app-root.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AppRootModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ proway-computers/src/app/produtos/produtos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduto, produtos } from '../produtos';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit{
+  produtos: IProduto[] = produtos;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
