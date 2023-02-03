cfa829a9f42672b5940c0bb21fef31b9bcc701f0
Kinnslayer <rodrigo.s.alencar@hotmail.com>
2023-02-03 03:31:10 -0300

    Java Fundamentos

---
+++ TipoDados/TipoDado/src/TipoDado.java
@@ -0,0 +1,16 @@
+public class TipoDado {
+    public static void main(String[] args) throws Exception {
+        //Demonstração de alguns tipos de dados
+        int numero;
+        float decimalCurto;
+        double decimalLongo;
+        char caracter;
+        String cadeiaCaracteres;
+
+        numero = 10;
+        decimalCurto = 10.5f;
+        decimalLongo = 20.5;
+        caracter = 'A';
+        cadeiaCaracteres = "Meu curso de Java Fundamentos em 2021"; 
+    }
+}
