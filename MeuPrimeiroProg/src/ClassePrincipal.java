36c244370aeb73b8da165bf64921e4ac647482b1
Rodrigo Alencar <119972623+Kinnslayer@users.noreply.github.com>
2023-02-03 03:41:09 -0300

    Delete MeuPrimeiroProg directory

--- MeuPrimeiroProg/src/ClassePrincipal.java
+++
@@ -1,8 +0,0 @@
-public class ClassePrincipal {
-    public static void main(String[] args) throws Exception {
-        
-        double numero = 855.6;
-        
-        System.out.println("O valor do núméro é: " + numero);
-    }
-}
